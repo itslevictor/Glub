a87bfd1a163174e0cfcaadb3e390477758dd25b1
Victor Fernando <adm.victorfernando@gmail.com>
2022-10-16 22:00:15 -0300

    Primeiras Paginas
    Criacao das primeiras paginas básicas, somente com botao para alterar cada uma.

---
+++ app/src/main/java/com/example/glub/Login.kt
@@ -0,0 +1,20 @@
+package com.example.glub
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.glub.databinding.ActivityLoginBinding
+import com.example.glub.databinding.ActivityMainBinding
+
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btMain.setOnClickListener{
+            val navegarMain = Intent(this, MainActivity::class.java)
+            startActivity(navegarMain)
+        }
+    }
+}
